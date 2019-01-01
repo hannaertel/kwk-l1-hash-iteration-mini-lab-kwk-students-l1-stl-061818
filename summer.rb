859dc0519085962450f8deceb0a0cb08383d390e
hannaertel <hannaertel@yahoo.com>
2019-01-01 04:25:25 +0000

    Automatically backed up by Learn

--- summer.rb
+++ summer.rb
@@ -5,7 +5,7 @@ def iteratehash
     "Beijing" => "2008",
     "London" => "2012"
   }
-  summerhash
+  summerhash["atlanta"]
   summerhash.each do |city, year|
     puts "The #{city} summer olympics took place in #{year}."
   end
